864fb86948d7ffc22b3ad73e994e81e91512be9d
Jamuna Prasad <jp1kashyap@gmail.com>
2025-01-10 23:22:06 +0530

    test cases

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,4 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import cors from "cors";
-
-import authRoutes from "./routes/auth.routes";
-import topicRoutes from "./routes/topic.routes";
-import progressRoutes from "./routes/progress.routes";
-
-dotenv.config();
-
-const app = express();
-
-// Middleware
-app.use(
-  cors({
-    origin: "https://dsa-sheet-fe.vercel.app", // Your frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-    credentials: true, // Include credentials if needed (e.g., cookies, HTTP auth)
-  })
-);
-app.use(bodyParser.json());
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.use("/api/auth", authRoutes);
-app.use("/api/topics", topicRoutes);
-app.use("/api/progress", progressRoutes);
-
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI as string, {})
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB Connection Error:", err));
-
+import app from "./app";
 // Start Server
 const PORT = process.env.PORT || 3010;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT);
